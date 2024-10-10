8a754426132b65ff55b7da1f785ddb6444972882
Matheus Kayque <matheuskayquebatistasilva712@gmail.com>
2024-10-10 00:12:28 -0300

    fix: Correção nos cards de Equipe tranferindo a rotação do Hover para um botão para ter compatibilidade melhor com mobile.

--- scripts.js
+++ scripts.js
@@ -3,12 +3,6 @@
 const btnsMenu = document.querySelectorAll('#btnMenu')
 const containerMenu = document.querySelector('.header_menu')
 
-/*Componentes do texto Sobre*/
-
-const btnsLerMais = document.querySelectorAll('#btn_ler_mais')
-const primeiroTexto = document.querySelector('.text_1')
-const segundoTexto = document.querySelector('.text_2')
-
 btnsMenu.forEach((btnMenu) => {
     btnMenu.addEventListener('click', () => {
         const menu = containerMenu.classList
@@ -23,6 +17,12 @@ btnsMenu.forEach((btnMenu) => {
     })
 })
 
+/*Componentes do texto Sobre*/
+
+const btnsLerMais = document.querySelectorAll('#btn_ler_mais')
+const primeiroTexto = document.querySelector('.text_1')
+const segundoTexto = document.querySelector('.text_2')
+
 btnsLerMais.forEach((btnMais) => {
     btnMais.addEventListener('click', () => {
         if (btnMais.classList.contains('ler_mais_1')) {
@@ -30,5 +30,22 @@ btnsLerMais.forEach((btnMais) => {
         } else {
             segundoTexto.classList.toggle('text_total')
         }
+    })
+})
+
+/*Componentes da seção Equipe*/
+const cardsEquipes = document.querySelectorAll('.section_equipe_contain_cards_card')
+
+cardsEquipes.forEach((cards) => {
+    const cardFront = cards.getElementsByClassName('card_flip_front')
+    const cardBack = cards.getElementsByClassName('card_flip_back')
+    const btns = cards.querySelectorAll('.ler_mais_equipe')
+
+    btns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            cards.classList.toggle('section_equipe_contain_cards_card_rotate')
+            cardFront.item(0).classList.toggle('card_flip_front_rotate')
+            cardBack.item(0).classList.toggle('card_flip_back_rotate')
+        })
     })
 })
